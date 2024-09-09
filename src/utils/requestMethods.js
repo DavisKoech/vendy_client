d111ca44539b11fe7916d01066b5f64d7eb6b7cb
DavisKoech <daviskoech94@gmail.com>
2024-09-09 15:53:21 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-//const BASE_URL = "https://vendy-api.onrender.com/api/"
-const BASE_URL = "http://localhost:8000/api/"
+const BASE_URL = "https://vendy-api-km85.onrender.com/api/"
+//const BASE_URL = "http://localhost:8000/api/"
 
 
 export const apiRequests = axios.create({
